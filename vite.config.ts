5810e059b3bf08fa4f526b89a5467e3052a4d189
Giridhar Thambisetty <giridhar368@gmail.com>
2025-12-30 22:47:18 +0530

    gh pages updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/shettyindustries/",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
